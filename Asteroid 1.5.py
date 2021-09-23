8b74c24646fc6a885d1466b6dab6cd0179707994
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-09-23 19:07:22 +0530

    Update Asteroid 1.5.py

--- Asteroid 1.5.py
+++ Asteroid 1.5.py
@@ -26,7 +26,10 @@ while True:
  
   enemy.x=enemy.x + xvel
  
-  if enemy.x < -250 or enemy.x > 650 :
+  if enemy.x < -250:
+    xvel = -1*xvel  
+  
+  if enemy.x > 650 :
     xvel = -1*xvel
   
   pygame.draw.rect(screen,BLUE,player)
